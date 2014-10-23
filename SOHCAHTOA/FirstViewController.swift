5e8c93496e745de0b41cbb392d96f83be73dc8ae
iMillJoe <josephmillion@me.com>
2014-10-22 19:49:27 -0500

    Initial Commit

---
+++ SOHCAHTOA/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SOHCAHTOA
+//
+//  Created by Joe Million on 10/22/14.
+//  Copyright (c) 2014 iMillIndustries. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
